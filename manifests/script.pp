4e913bd03f7b4c501568b8288304d7104955e3a1
Dean Taylor <dean@gohitech.net>
2017-09-08 10:49:36 +0800

    added default values for pam entry cleaned up winbind code; removed unused define\ removed some development code from pam::winbind class

--- manifests/script.pp
+++ manifests/script.pp
@@ -1,5 +1,10 @@
 class pam::script (
+  $onerr = 'fail',	# onerr=fail|success
+  $dir   = undef,	# dir=/usr/share/libpam-script
 ) {
+  validate_re($onerr,'fail|success')
+  if $dir { validate_path($dir) }
+
   package { 'pam_script':
     ensure => present,
   }
--- manifests/winbind.pp
+++ manifests/winbind.pp
@@ -1,44 +1,8 @@
-define pam::d::winbind (
-  $control,
-  $service,
-  $type,
-  $debug   = undef,
-  $debug_state = undef,
-  $enable_cached_login   = false,
-  $enable_krb5_auth      = false,
-  $enable_mkhomedir      = false,
-  $krb5_ccache_type      = '',
-  $require_membership_of = [],
-  $silent                = false,
-) {
-  if ! defined(Concat["/etc/pam.d/${service}"]) {
-    concat { "/etc/pam.d/${service}":
-      ensure  => present,
-      content => template('pam/pam.d/winbind.erb'),
-      warn    => true,
-    }
-  }
-
-#  concat::fragment { "/etc/pam.d/${service}_${type}_winbind":
-}
-
-define pam::winbind::service (
-  $rules,
-  $service = $name,
-) {
-  validate_array($rules)
-  validate_string($service)
-
-  $_rules = ght_merge_each($rules,{service=>$service,})
-
-  ::pam::service::rule { $_rules: }
-}
-
 class pam::winbind (
   $cached_login          = true,
   $krb5_auth             = true,
   $krb5_ccache_type      = $pam::params::winbind_krb5_ccache_type,
-  $require_membership_of = undef,		# 'UNIWA\SCI-NIX-029$,UNIWA\00078081,UNIWA\90078081'
+  $require_membership_of = undef,			# e.g., 'UNIWA\SCI-NIX-029$,UNIWA\00078081,UNIWA\90078081'
   $services              = [ 'system-auth-puppet', ],	# undef
   $packages              = $pam::params::winbind_packages,
 ) inherits pam::params {
@@ -66,14 +30,7 @@ class pam::winbind (
         ]
       }
     }
-    create_resources(pam::service,$_services)
 
-#    $_rules = [
-#      { service=>'system-auth-puppet',type=>'auth',module=>'winbind',control=>'sufficient',args=>concat($_cached_login,$_krb5_auth,$_require_membership_of,['use_first_pass']) },
-#      { service=>'system-auth-puppet',type=>'account',module=>'winbind',control=>'[default=bad success=ok user_unknown=ignore]',args=>concat($_cached_login,$_krb5_auth) },
-#      { service=>'system-auth-puppet',type=>'password',module=>'winbind',control=>'sufficient',args=>concat($_krb5_auth,['use_authtok']) },
-#      { service=>'system-auth-puppet',type=>'session',module=>'winbind',control=>'optional',args=>concat($_cached_login,$_krb5_auth) },
-#    ]
-#    ::pam::service::rule { $_rules: }
+    create_resources(pam::service,$_services)
   }
 }
